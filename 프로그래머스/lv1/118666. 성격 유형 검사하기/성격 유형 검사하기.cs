12867082163392c43e33e11887b9255308c8bf67
DongKyun Noh <shehdrbs123@naver.com>
2023-09-12 09:51:10 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 3.55 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
@@ -1,15 +1,59 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 public class Solution {
     public string solution(string[] survey, int[] choices) {
         string answer = "";
-        Dictionary<string,int> surveySum = new Dictionary<string,int>();
         
-        MakeResult(surveySum,survey,choices);
-        MakeYourStyle(surveySum,out answer);
+        answer = Solve(survey,choices);
         
         return answer;
     }
+    //현재 풀이
+    private string Solve(string[] survey, int[] choices)
+    {
+        Dictionary<string,int> scoreCount = new Dictionary<string,int>();
+        string[] personality = new string[]{ "RT","CF","JM","AN"};
+        char[] result = new char[4];
+        for(int i=0;i<survey.Length;++i)
+        {
+            int score = 0;
+            string selected = survey[i];
+            int currentScore = choices[i]-4;
+            if(scoreCount.TryGetValue(selected,out score))
+            {
+                scoreCount[selected] += currentScore;
+                
+            }else
+            {
+                scoreCount.Add(selected, currentScore);
+            }
+        }
+        
+        for(int i=0;i<personality.GetLength(0); ++i)
+        {
+            string sit1 = personality[i];
+            string sit2 = new string(sit1.Reverse().ToArray());
+            
+            int sit1Score = 0;
+            int sit2Score = 0;
+            scoreCount.TryGetValue(sit1, out sit1Score);
+            scoreCount.TryGetValue(sit2, out sit2Score);            
+            //Console.WriteLine(sit1Score + " " + sit2Score);           
+            int resultScore = sit1Score - sit2Score;
+            
+            if(resultScore>0)
+                result[i] = sit1[1];
+            else
+                result[i] = sit1[0];
+        }
+        return new string(result);
+    }
+    
+    
+    
+    
+    //이전의 풀이
     private void MakeResult( Dictionary<string,int> surveySum,string[] survey, int[] choices)
     {
         for(int i=0;i<survey.Length;i++)
