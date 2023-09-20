ff8e5224ca09875453576a84580325a9498780b3
DongKyun Noh <shehdrbs123@naver.com>
2023-09-20 10:02:17 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.20 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cs
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cs
@@ -7,15 +7,12 @@ public class Solution {
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
-        dp[3] = 3;
-        dp[4] = 5;
         
-        for(int i=5;i<=2000;++i)
+        for(int i=3;i<=n;++i)
         {
             dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
             
         }
-        Console.WriteLine(dp[6]);
         answer = dp[n];
         
         return answer;
